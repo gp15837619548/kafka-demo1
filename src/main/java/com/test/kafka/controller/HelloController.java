cb53d4649a3dd504207b00b5ee6da51536c50b1f
gp_develop <gp15837619548@163.com>
2021-06-30 16:51:51 +0800

    Idea提交代码

--- src/main/java/com/test/kafka/controller/HelloController.java
+++ src/main/java/com/test/kafka/controller/HelloController.java
@@ -1,19 +1,7 @@
 package com.test.kafka.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-
-    @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
-
-    @GetMapping("/hello")
-    public String hello(){
-        kafkaTemplate.send("kafka-hello","kafka练习");
-        return "ok";
+    public static void main(String[] args) {
+        System.out.println("hello Git");
     }
 }
